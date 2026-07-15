98a3d0b1ee36c27159cc3369f591a16d113d8eec
Kasturi Harshitha <harshithakasturi4@gmail.com>
2026-07-15 11:56:53 +0530

    Delete CashFlow-main directory

--- CashFlow-main/frontend/src/routes/root.tsx
+++
@@ -1,6 +0,0 @@
-import { createRootRoute } from "@tanstack/react-router";
-import { Outlet } from "@tanstack/react-router";
-
-export const rootRoute = createRootRoute({
-  component: () => <Outlet />
-});
