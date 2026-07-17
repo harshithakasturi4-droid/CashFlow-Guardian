71cca87070bd8aca3cbc05dc134269b70d5eefa1
Kasturi Harshitha <harshithakasturi4@gmail.com>
2026-07-17 17:43:54 +0530

    Change routing from 'virtual' to 'hash' in SignUp

--- frontend/src/routes/sign-up.tsx
+++ frontend/src/routes/sign-up.tsx
@@ -55,7 +55,7 @@ function SignUpPage() {
           </div>
 
           <SignUp
-            routing="virtual"
+            routing="hash"
             signInUrl="/auth/sign-in"
             fallbackRedirectUrl="/"
           />
